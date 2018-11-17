fbd7a8a40d477917059b8475daf7bcc16a6b197a
Arkadiusz Ryszewski <arkadiusz.ryszewski@sollers.eu>
2018-11-17 13:43:50 +0100

    make test classes more interesting

---
+++ testingProject/src/main/java/kendal/experiments/MainClass.java
@@ -0,0 +1,9 @@
+package kendal.experiments;
+
+public class MainClass {
+
+    public static void main() {
+        TestClass testClass = new TestClass(12, 15);
+        testClass.b = 15;
+    }
+}
--- testingProject/src/main/java/kendal/experiments/TestClass.java
+++ testingProject/src/main/java/kendal/experiments/TestClass.java
@@ -1,13 +1,18 @@
 package kendal.experiments;
 
 import kendal.annotations.Private;
-import kendal.annotations.Protected;
+import kendal.annotations.Public;
 
 public class TestClass {
     int x;
 
-    public TestClass(@Private int a, @Protected int b) {
+    public TestClass(@Private int a, @Public int b) {
         this.x = a;
         System.out.println("Let's have here some expression statement");
+    }
+
+    private int someMethod() {
+        a = 15;
+        return a;
     }
 }
