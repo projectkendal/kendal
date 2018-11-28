e417829ba70e0ce8ca6c3515a37a93a48df49e4c
Arkadiusz Ryszewski <arkadiusz.ryszewski@sollers.eu>
2018-11-28 23:27:31 +0100

    [@Clone] refactor CloneHandler class

--- handlers/src/main/java/kendal/handlers/CloneHandler.java
+++ handlers/src/main/java/kendal/handlers/CloneHandler.java
@@ -37,39 +37,36 @@ public class CloneHandler implements KendalHandler<Clone> {
         }
     }
 
-    public void handleNode(Node annotationNode, AstHelper helper) throws KendalException {
+    private void handleNode(Node annotationNode, AstHelper helper) throws KendalException {
         Node<JCMethodDecl> method = (Node<JCMethodDecl>) annotationNode.getParent();
         Node<JCClassDecl> clazz = (Node<JCClassDecl>) method.getParent();
         JCMethodDecl m = method.getObject();
-        Name cloneMethodName = getCloneMethodName(m.name.toString(), annotationNode.getParent(), clazz);
+        Name cloneMethodName = getCloneMethodName(m.name.toString(), annotationNode.getParent());
+        validateMethodIsUnique(cloneMethodName, m.params, clazz);
         JCModifiers modifiers = getModifiersForNewMethod(m);
         // todo: run original method in clone method's body
         Node<JCMethodDecl> cloneMethod = astNodeBuilder.buildMethodDecl(modifiers, cloneMethodName, m.restype, m.params, m.body);
         helper.addElementToClass(clazz, cloneMethod, Mode.APPEND);
     }
 
-    private Name getCloneMethodName(String originMethodName, Node<JCMethodDecl> clonedMethod, Node<JCClassDecl> clazz)
-            throws DuplicatedElementsException {
+    private Name getCloneMethodName(String originMethodName, Node<JCMethodDecl> clonedMethod) {
         String proposedName = clonedMethod.getObject().sym.getAnnotation(Clone.class).methodName();
+        String newMethodName = !Objects.equals("", proposedName) ? proposedName : originMethodName + "Clone";
+        return astUtils.nameFromString(newMethodName);
+    }
+
+    private void validateMethodIsUnique(Name methodName, List<JCVariableDecl> params, Node<JCClassDecl> clazz)
+            throws DuplicatedElementsException {
+        String name = methodName.toString();
         Set<JCMethodDecl> allMethods = clazz.getChildrenOfType(JCMethodDecl.class).stream()
                 .map(Node::getObject).collect(Collectors.toSet());
-        String newMethodName = !Objects.equals("", proposedName) ? proposedName : originMethodName + "Clone";
         for (JCMethodDecl method : allMethods) {
-            if (method.name.toString().equals(newMethodName)
-                    && collectionsOfParametersEqualByValues(method.params, clonedMethod.getObject().params)) {
+            if (method.name.toString().equals(name)
+                    && collectionsOfParametersEqualByValues(method.params, params)) {
                 throw new DuplicatedElementsException("Clone method cannot be created because there already exists a"
                         + " method with such declaration!");
             }
         }
-        return astUtils.nameFromString(newMethodName);
-    }
-
-    private JCModifiers getModifiersForNewMethod(JCMethodDecl methodDecl) {
-        JCModifiers newModifiers = (JCModifiers) methodDecl.mods.clone();
-        // Reset annotations
-        newModifiers.annotations = com.sun.tools.javac.util.List.from(new ArrayList<>());
-        // todo: add annotations based on @Clone annotation parameter
-        return newModifiers;
     }
 
     private boolean collectionsOfParametersEqualByValues(List<JCVariableDecl> params1, List<JCVariableDecl> params2) {
@@ -80,6 +77,14 @@ public class CloneHandler implements KendalHandler<Clone> {
                         p1.name == p2.name && Objects.equals(p1.vartype.toString(), p2.vartype.toString())
                 )
         );
+    }
+
+    private JCModifiers getModifiersForNewMethod(JCMethodDecl methodDecl) {
+        JCModifiers newModifiers = (JCModifiers) methodDecl.mods.clone();
+        // Reset annotations
+        newModifiers.annotations = com.sun.tools.javac.util.List.from(new ArrayList<>());
+        // todo: add annotations based on @Clone annotation parameter
+        return newModifiers;
     }
 
     @Override
