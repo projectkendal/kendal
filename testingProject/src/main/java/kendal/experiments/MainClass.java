c35aaceac1213a1a35a8e6f11155b864e3d1016d
Arkadiusz Ryszewski <arkadiusz.ryszewski@sollers.eu>
2018-12-03 22:24:33 +0100

    [improvement] modify testing project to enable easy compilation testing

--- testingProject/src/main/java/kendal/experiments/MainClass.java
+++ testingProject/src/main/java/kendal/experiments/MainClass.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 public class MainClass {
 
-    public static void main() {
+    public static void main(String[] args) {
         TestClass testClass = new TestClass(69, new ArrayList<>(), null);
+        CloneTest cloneTest = new CloneTest();
         List var = testClass.b;
         int var2 = testClass.a;
         testClass.b = new ArrayList<>();
+        System.out.println(var2);
+        System.out.println(cloneTest.transformedMethod("text", 123));
     }
 }
--- testingProject/src/main/java/kendal/experiments/subpackage/TestClassTransformer.java
+++ testingProject/src/main/java/kendal/experiments/subpackage/TestClassTransformer.java
@@ -4,14 +4,14 @@ import java.io.Serializable;
 
 import kendal.annotations.Clone;
 
-public class TestClassTransformer extends Subclass implements Serializable, Clone.Transformer<String, Integer> {
+public class TestClassTransformer extends Subclass implements Serializable, Clone.Transformer<String, String> {
     @Override
     public int transform(long param) {
         return (int) param;
     }
 
     @Override
-    public Integer transform(String input) {
-        return 15;
+    public String transform(String input) {
+        return input + "LOL";
     }
 }
