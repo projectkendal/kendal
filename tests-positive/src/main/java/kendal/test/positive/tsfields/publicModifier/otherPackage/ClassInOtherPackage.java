95e9164c70b501503514a211fa866c75d57f682f
Arkadiusz Ryszewski <arkadiusz.ryszewski@sollers.eu>
2018-12-08 23:09:06 +0100

    [@TsFields] rename in tests: "newClass" -> "viaNewClass", "identifier" -> "viaIdentifier"

--- tests-positive/src/main/java/kendal/test/positive/tsfields/publicModifier/otherPackage/ClassInOtherPackage.java
+++ tests-positive/src/main/java/kendal/test/positive/tsfields/publicModifier/otherPackage/ClassInOtherPackage.java
@@ -10,40 +10,40 @@ public class ClassInOtherPackage {
 
     // ### Test cases ###
 
-    private int shouldAccessField_primitive_newClass() {
+    private int shouldAccessField_primitive_viaNewClass() {
         return new ClassWithFieldsGenerated(i(), l(), i(), l()).primitiveField;
     }
 
-    private int shouldAccessAndModifyField_primitive_identifier() {
+    private int shouldAccessAndModifyField_primitive_viaIdentifier() {
         ClassWithFieldsGenerated classWithFieldsGenerated = new ClassWithFieldsGenerated(i(), l(), i(), l());
         classWithFieldsGenerated.primitiveField = i();
         return classWithFieldsGenerated.primitiveField;
     }
 
-    private int shouldAccessField_list_newClass() {
+    private int shouldAccessField_list_viaNewClass() {
         return new ClassWithFieldsGenerated(i(), l(), i(), l()).listField.size();
     }
 
-    private int shouldAccessAndModifyField_list_identifier() {
+    private int shouldAccessAndModifyField_list_viaIdentifier() {
         ClassWithFieldsGenerated classWithFieldsGenerated = new ClassWithFieldsGenerated(i(), l(), i(), l());
         classWithFieldsGenerated.listField = l();
         return classWithFieldsGenerated.listField.size();
     }
 
-    private int shouldAccessField_primitiveFinal_newClass() {
+    private int shouldAccessField_primitiveFinal_viaNewClass() {
         return new ClassWithFieldsGenerated(i(), l(), i(), l()).primitiveField;
     }
 
-    private int shouldAccessField_primitiveFinal_identifier() {
+    private int shouldAccessField_primitiveFinal_viaIdentifier() {
         ClassWithFieldsGenerated classWithFieldsGenerated = new ClassWithFieldsGenerated(i(), l(), i(), l());
         return classWithFieldsGenerated.primitiveField;
     }
 
-    private int shouldAccessField_listFinal_newClass() {
+    private int shouldAccessField_listFinal_viaNewClass() {
         return new ClassWithFieldsGenerated(i(), l(), i(), l()).listField.size();
     }
 
-    private int shouldAccessField_listFinal_identifier() {
+    private int shouldAccessField_listFinal_viaIdentifier() {
         ClassWithFieldsGenerated classWithFieldsGenerated = new ClassWithFieldsGenerated(i(), l(), i(), l());
         return classWithFieldsGenerated.listField.size();
     }
