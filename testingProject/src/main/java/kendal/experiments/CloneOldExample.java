0b2c44b90e2edb934a051e9e111a907cc610244d
Konrad Gancarz <konrad.gancarz9@gmail.com>
2018-12-26 16:56:35 +0100

    uncomment clone old example

--- testingProject/src/main/java/kendal/experiments/CloneOldExample.java
+++ testingProject/src/main/java/kendal/experiments/CloneOldExample.java
@@ -9,22 +9,22 @@ import java.util.List;
 
 public class CloneOldExample {
 
-//    @Clone(transformer = CsvTransformer.class, onMethod = {@RequestMapping(value = "/method1/csv", method = "POST")})
-//    @RequestMapping(value = "/method1", method = "POST")
-//    public List<Object> method1() {
-//        return new ArrayList<>();
-//    }
-//
-//    @Clone(transformer = CsvTransformer.class, onMethod = {@RequestMapping(value = "/method2/csv", method = "POST")})
-//    @RequestMapping(value = "/method2", method = "POST")
-//    public List<Object> method2() {
-//        return new ArrayList<>();
-//    }
-//
-//    @Clone(transformer = CsvTransformer.class, onMethod = {@RequestMapping(value = "/method3/csv", method = "POST")})
-//    @RequestMapping(value = "/method3", method = "POST")
-//    public List<Object> method3() {
-//        return new ArrayList<>();
-//    }
+    @Clone(transformer = CsvTransformer.class, onMethod = {@RequestMapping(value = "/method1/csv", method = "POST")})
+    @RequestMapping(value = "/method1", method = "POST")
+    public List<Object> method1() {
+        return new ArrayList<>();
+    }
+
+    @Clone(transformer = CsvTransformer.class, onMethod = {@RequestMapping(value = "/method2/csv", method = "POST")})
+    @RequestMapping(value = "/method2", method = "POST")
+    public List<Object> method2() {
+        return new ArrayList<>();
+    }
+
+    @Clone(transformer = CsvTransformer.class, onMethod = {@RequestMapping(value = "/method3/csv", method = "POST")})
+    @RequestMapping(value = "/method3", method = "POST")
+    public List<Object> method3() {
+        return new ArrayList<>();
+    }
 
 }
