9655c3f84824885af4f7219cb744bb698007682a
Arkadiusz Ryszewski <arkadiusz.ryszewski@sollers.eu>
2019-01-05 16:16:18 +0100

    - [core] remove todo that was already done

--- processor/src/main/java/kendal/api/KendalHandler.java
+++ processor/src/main/java/kendal/api/KendalHandler.java
@@ -22,7 +22,6 @@ public interface KendalHandler<T extends Annotation> {
      */
     void handle(Collection<Node> annotationNodes, AstHelper helper) throws KendalException;
 
-    // TODO provide default implementation searching for type parameters https://trello.com/c/RSuRWZNp/42-provide-default-implementation-searching-for-type-parameters-in-kendalhandlerjava
     /**
      * Returns annotation that given handler supports. If returns null, it means this handler's functionality is not
      * based on any annotation and it should get all AST trees roots.
